f9306fd558e93b3acaaaa5befe5840696fde5c82
Ryan Adams <ryan.g.adams@gmail.com>
2012-09-15 17:29:39 +0100

    components listed on the left, demo pages (on int) open in an iframe on the right

--- explorer.js
+++ explorer.js
@@ -1,20 +1,36 @@
 
 function load() {                 
-  console.log("loading");
+  var container = document.createElement("ul");
+  container.setAttribute("id", "component-list");
+  document.body.appendChild(container);
   for (var i = components["components"].length - 1; i >= 0; i--){
-    var el = document.createElement("p");             
+    var el = document.createElement("li");             
     var t = components["components"][i];
-    var content = '<a class="code" href="' + t["url"] + '">' + t["code"] + "</a>";
+    var content = '<span><a class="code" href="' + t["url"] + '">' + t["code"] + "</a>";
     if (t["jira"]) {                   
       var jira = t["jira"].split("/").pop();
       content += '<a class="jira" href="' + t["jira"] + '">' + jira + "</a>";
     }
-    content += '<a href="https://pal.int.bbc.co.uk/kandlroute/developers/staticlibrary/'+t["code"]+'">Library</a>';
-    content += t["name"];
+    content += t["name"] + "</span>";
     el.innerHTML = content;
     el.className = t["status"];
-    document.body.appendChild(el);
-  };
+    container.appendChild(el);
+    el.setAttribute("data-json", JSON.stringify(t));
+    el.addEventListener("click", function(){
+      json = JSON.parse(this.getAttribute("data-json"));
+      document.getElementById("explorer").setAttribute(
+          "src", 
+          'https://pal.int.bbc.co.uk/kandlroute/developers/staticlibrary/'+json["code"]
+      );
+      return false;
+    }, false);
+                                       
+  };                                   
+  
+  var pane = document.createElement("iframe");
+  pane.setAttribute("id","explorer");
+  document.body.appendChild(pane);                         
+  pane.appendChild(document.createTextNode("Details go here"));
 }
 
 document.addEventListener("DOMContentLoaded", load, false);
