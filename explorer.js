05560a54afd7b734b8ff0fd57d71b9cfcd531818
Ryan Adams <ryan.g.adams@gmail.com>
2012-09-17 23:39:47 +0100

    another couple of functions to improve code readability.

--- explorer.js
+++ explorer.js
@@ -8,79 +8,81 @@ var make = function(element) {
   return document.createElement(element);
 };
 
-function load() {                 
-  var container = make("ul");
-  container.setAttribute("id", "component-list");
-  document.body.appendChild(container);
+function load() {
+  var container = buildAndAdd("ul", "component-list", "");
+  buildAndAdd('h1', "component-name", "Component Library");
+  buildAndAdd('div', 'component-summary', '');
+  buildAndAdd("iframe", "explorer", "");
+  setUpBreakpoints();
+
+  sortComponentsByCode();
+  for (var i = components["components"].length - 1; i >= 0; i--){
+    container.appendChild(makeComponentItem(components["components"][i]));
+  };
+}
+function buildAndAdd(element, id, innerHTML) {
+  var el = make(element);
+  el.setAttribute("id", id);
+  el.innerHTML = innerHTML
+  document.body.appendChild(el);
+  return el;
+}
+function sortComponentsByCode() {
   components["components"].sort(function(a, b){
     return a["code"] < b["code"] ? 1 : -1;
   });
-  for (var i = components["components"].length - 1; i >= 0; i--){
-    var el = make("li");             
-    var t = components["components"][i];
-    var content = '<span><a class="code" href="' + t["url"] + '">' + t["code"] + "</a>";
-    content += t["name"] + "</span>";
-    el.innerHTML = content;
-    el.className = t["status"];
-    container.appendChild(el);
-    el.setAttribute("data-json", JSON.stringify(t));
-    el.addEventListener("click", showComponentDetail, false);
-                                       
-  };                                   
-  var title = make("h1");
-  title.setAttribute("id", "component-name");
-  title.innerHTML = "Component Library";
-  document.body.appendChild(title);
-  var summary = make("div");
-  summary.setAttribute("id", "component-summary");
-  document.body.appendChild(summary);
-  var pane = make("iframe");
-  pane.setAttribute("id","explorer");
-  document.body.appendChild(pane);                         
-  pane.appendChild(document.createTextNode("Details go here")); 
-  setUpBreakpoints();
 }
-                                                           
+function makeComponentItem(t) {
+  var el = make("li");
+  var content = '<span><a class="code" href="' + t["url"] + '">' + t["code"] + "</a>";
+  content += t["name"] + "</span>";
+  el.innerHTML = content;
+  el.className = t["status"];
+  el.setAttribute("data-json", JSON.stringify(t));
+  el.addEventListener("click", showComponentDetail, false);
+  return el;
+}
+
 
 function showComponentDetail(e) {
   e.preventDefault();
   hidePreviewPaneAndShowOverview();
   json = JSON.parse(this.getAttribute("data-json"));
-                                                        
-  var content = "Component: <span class=\"code\">" + json["code"] + 
+
+  var content = "Component: <span class=\"code\">" + json["code"] +
     "</span> " + json["name"] + "<p>" +
-    "<a class=\"overview\" href=\"overview\">Overview</a>&nbsp;|&nbsp;" + 
+    "<a class=\"overview\" href=\"overview\">Overview</a>&nbsp;|&nbsp;" +
     "<a href=\"" + json["url"] + "\">Confluence</a>&nbsp;|&nbsp;";
   if (json["cucumber"].slice(0,4) == "http") {
-    content += "<a class=\"component-ac open-in-iframe\" " + 
+    content += "<a class=\"component-ac open-in-iframe\" " +
     "href=\"" + json["cucumber"] + "\">" +
     "Acceptance Criteria</a>&nbsp;|&nbsp;";
   } else {
     content += "No link to cucumber&nbsp;|&nbsp;";
   }
-  content += "<a class=\"component-preview open-in-iframe\" " + 
+  content += "<a class=\"component-preview open-in-iframe\" " +
     "href=\"https://pal.int.bbc.co.uk/kandlroute/developers/staticlibrary/" + json["code"] + "\">" +
     "Test Harness</a>&nbsp;|&nbsp;";
-  if (json["jira"]) {                   
+  if (json["jira"]) {
     var jira = json["jira"].split("/").pop();
     content += '<a class="jira" href="' + json["jira"] + '">' + jira + "</a>";
-  }          
-  content += "</p>";                       
-  var summary = "<p>" + json["overview"] + "</p>";                 
-  if (json["design"].length > 0) {           
+  }
+  content += "</p>";
+  var summary = "<p>" + json["overview"] + "</p>";
+  if (json["design"].length > 0) {
     summary += "<h2>Design Samples</h2>";
     for (var i=0; i < json["design"].length; i++) {
       var img = json["design"][i];
       var imgname = img.split("/").pop().split("?").shift();
-      
+
       summary += "<p>" + decodeURIComponent(imgname) + "</p><a href=\""+img+"\"><img src=\"" + img + "\"/></a>"
     };
   }
   get("#component-name").innerHTML = content;
   get("#component-summary").innerHTML = summary;
- 
+
   setUpOverviewLink();
-  setUpOpenInIframe();  
+  setUpOpenInIframe();
 }
 
 function setUpOpenInIframe() {
@@ -90,7 +92,7 @@ function setUpOpenInIframe() {
   };
 }
 
-function openLinkInIframe(e) { 
+function openLinkInIframe(e) {
   var iframe = get("#explorer");
   showPreviewPaneAndHideOverview();
   if (this.className.indexOf("-ac") > -1) {
@@ -102,7 +104,7 @@ function openLinkInIframe(e) {
     get("#resize-320").click();
   }
   iframe.setAttribute("src", this.getAttribute("href"));
-  e.preventDefault();                                                                
+  e.preventDefault();
 }
 
 /* make the iframe resize to various widths */
@@ -111,7 +113,7 @@ function setUpBreakpoints() {
   var b = make("p");
   b.appendChild(document.createTextNode("Resize Preview: "));
   b.setAttribute("id", "breakpoint-container");
-  for (var i = breakpoints.length - 1; i >= 0; i--){ 
+  for (var i = breakpoints.length - 1; i >= 0; i--){
     var l = make("a");
     l.appendChild(document.createTextNode(breakpoints[i]));
     l.setAttribute("href", "#resize-" + breakpoints[i]);
@@ -120,8 +122,8 @@ function setUpBreakpoints() {
     l.addEventListener("click", resize, false);
   };
   document.body.appendChild(b);
-}                                              
-          
+}
+
 function setUpOverviewLink() {
   link = document.querySelector("a.overview");
   link.addEventListener("click", function(e){
@@ -147,7 +149,7 @@ function showPreviewPaneAndHideOverview() {
   var iframe = get("#explorer");
   iframe.style.visibility = "visible";
   var resizer = get("#breakpoint-container");
-  resizer.style.visibility = "visible";             
+  resizer.style.visibility = "visible";
   var summary = get("#component-summary");
   summary.style.visibility = "hidden";
 }
